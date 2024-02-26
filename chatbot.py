c91a391deffce97a381e1c88c3a9cf1f69bb5a10
krisograbek <krzysztof.ograbek@gmail.com>
2024-02-26 05:27:30 +0100

    update for openai v1, add flags

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,13 @@
 import streamlit as st
-import openai
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-openai.api_key = os.environ["OPENAI_API_KEY"]
+# openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# initialize the OpenAI client
+client = OpenAI()
 
 # Sidebar
 st.sidebar.title("Configuration")
@@ -16,11 +19,16 @@ def model_callback():
 
 if "model" not in st.session_state:
     st.session_state["model"] = "gpt-3.5-turbo"
+    
+models = [
+    'gpt-4-turbo-preview',
+    'gpt-3.5-turbo',
+]
 
 st.session_state.model = st.sidebar.radio(
     "Select OpenAI Model",
-    ("gpt-3.5-turbo", "gpt-3.5-turbo-16k"),
-    index=0 if st.session_state["model"] == "gpt-3.5-turbo" else 1,
+    ('gpt-4-turbo-preview', 'gpt-3.5-turbo'),
+    index=1 if st.session_state["model"] == "gpt-3.5-turbo" else 0,
     on_change=model_callback,
     key="model_selected",
 )
@@ -34,25 +42,25 @@ st.sidebar.markdown(
 
 # Bot roles and their respective initial messages
 bot_roles = {
-    "English": {
+    "🇬🇧 English": {
         "role": "system",
         "content": "You are a friendly assistant",
-        "description": "This is a standard ChatGPT model.",
+        "description": "🇬🇧 This is a standard ChatGPT model.",
     },
-    "Polish bot": {
+    "🇵🇱 Polish bot": {
         "role": "system",
         "content": "You are a friendly bot that speaks only Polish",
-        "description": "This is a friendly bot speaking in Polish.",
+        "description": " 🇵🇱 This is a friendly bot speaking in Polish.",
     },
-    "German bot": {
+    "🇩🇪 German bot": {
         "role": "system",
         "content": "You are a friendly bot that speaks only German",
-        "description": "This is a friendly bot speaking in German.",
+        "description": "🇩🇪 This is a friendly bot speaking in German.",
     },
-    "English Pirate bot": {
+    "🏴‍☠️ English Pirate bot": {
         "role": "system",
         "content": "You are a friendly bot that speaks only English Pirate",
-        "description": "This is a friendly bot speaking in English Pirate.",
+        "description": "🏴‍☠️ This is a friendly bot speaking in English Pirate.",
     },
 }
 
@@ -61,7 +69,7 @@ def bot_role_callback():
     st.session_state["messages"] = [bot_roles[st.session_state["bot_role"]]]
 
 if "bot_role" not in st.session_state:
-    st.session_state["bot_role"] = "English"
+    st.session_state["bot_role"] = "🇬🇧 English"
 
 st.session_state.bot_role = st.sidebar.radio(
     "Select bot role",
@@ -110,17 +118,18 @@ if user_prompt := st.chat_input("Your prompt"):
         message_placeholder = st.empty()
         full_response = ""
 
-        for response in openai.ChatCompletion.create(
+        stream = client.chat.completions.create(
             model=st.session_state.model,
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
             stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
+        )
+        for chunk in stream:
+            if chunk.choices[0].delta.content is not None:
+                full_response += chunk.choices[0].delta.content
+                message_placeholder.markdown(full_response + "▌")
 
         message_placeholder.markdown(full_response)
 
